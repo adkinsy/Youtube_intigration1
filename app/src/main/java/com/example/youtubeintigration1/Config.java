bc055dd3c1b55544f89327ebfc0957c6c2bc7008
Aditya sharma <adkinsy85@gmail.com>
2020-01-13 14:23:39 +0530

    Update Config.java
    Add your API key

--- app/src/main/java/com/example/youtubeintigration1/Config.java
+++ app/src/main/java/com/example/youtubeintigration1/Config.java
@@ -4,5 +4,5 @@ public  final class Config {
         private Config() {
         }
 
-        public static final String YOUTUBE_API_KEY = "AIzaSyCFhnY9PiVJlw5mn9fxG8yPKJ8Gb6iALLw";
+        public static final String YOUTUBE_API_KEY = "fctctctctcctctctc";
 }
