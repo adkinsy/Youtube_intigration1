2e2a17c1bfaed1901f4ce1a347dfd2347f603e64
Aditya <adkinsy85@gmail.com>
2020-01-13 09:38:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/youtubeintigration1/Config.java
@@ -0,0 +1,8 @@
+package com.example.youtubeintigration1;
+
+public  final class Config {
+        private Config() {
+        }
+
+        public static final String YOUTUBE_API_KEY = "AIzaSyCFhnY9PiVJlw5mn9fxG8yPKJ8Gb6iALLw";
+}
